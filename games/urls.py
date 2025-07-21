2127f3ed96b7fb1bb0afe9d3af93d931ce39b550
Pawel Beer <pawel.beer@non.keysight.com>
2025-07-21 17:58:29 +0200

    added Next/Past games categories

--- games/urls.py
+++ games/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.all_games, name='all_games_url'),
+    path('', views.next_games, name='next_games_url'),
+    path('past/', views.past_games, name='past_games_url'),
     path('<game_id>/', views.game_details, name='game_details_url'),
 ]
--- games/views.py
+++ games/views.py
@@ -1,13 +1,18 @@
+from datetime import datetime
+
 from django.shortcuts import render
 
 from .models import Game
 
 
 # Create your views here.
-def all_games(request):
-    found_games = Game.objects.all()
-    return render(request, 'games/games_all.html', {'games': found_games})
+def next_games(request):
+    found_games = Game.objects.filter(when__gte=datetime.today()).order_by('when').all()
+    return render(request, 'games/next_games.html', {'games': found_games})
 
+def past_games(request):
+    found_games = Game.objects.filter(when__lt=datetime.today()).order_by('-when').all()
+    return render(request, 'games/past_games.html', {'games': found_games})
 def game_details(request, game_id):
     found_game = Game.objects.get(id=game_id)
     return render(request, 'games/game_details.html', {"game": found_game})
