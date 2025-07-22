9bd50484271d6dd32747ae091d589e71282f878e
Pawel Beer <pawel.beer@non.keysight.com>
2025-07-22 10:25:08 +0200

    added Players details

--- players/urls.py
+++ players/urls.py
@@ -2,4 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('players/', views.all_players, name='all_players_url'),
+    path('<player_id>/', views.player_details, name='player_details_url'),
+
 ]
--- players/views.py
+++ players/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
 from .models import Player
-
+from django.db.models import Avg, Min, Max, Count
 
 # Create your views here.
 def all_players(request):
     found_players = Player.objects.all()
-    return render(request, 'players/all_players.html', {'players': found_players})
+    found_players_aggregation = found_players.aggregate(
+        Count('id')
+    )
+    return render(request, 'players/all_players.html', {
+        'players': found_players,
+        'details': found_players_aggregation
+    })
+
+def player_details(request, player_id):
+    found_player = Player.objects.get(id=player_id)
+    return render(request, 'players/player_details.html', {"player": found_player})
