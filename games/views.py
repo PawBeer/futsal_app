964a0f839c1b9a4d30414cc03676c8ff231fd0c6
Pawel Beer <pawel.beer@non.keysight.com>
2025-07-22 11:38:48 +0200

    breadcrumbs and Futsal logo added

--- games/views.py
+++ games/views.py
@@ -1,9 +1,12 @@
 from datetime import datetime
-
 from django.shortcuts import render
-
 from .models import Game
+from django.urls import reverse
 
+class Breadcrumb:
+    def __init__(self, path, label):
+        self.path = path
+        self.label = label
 
 # Create your views here.
 def next_games(request):
@@ -15,4 +18,11 @@ def past_games(request):
     return render(request, 'games/past_games.html', {'games': found_games})
 def game_details(request, game_id):
     found_game = Game.objects.get(id=game_id)
-    return render(request, 'games/game_details.html', {"game": found_game})
+    return render(request, 'games/game_details.html', {
+        "game": found_game,
+        "breadcrumbs": [
+            Breadcrumb(reverse('next_games_url'), 'Next games'),
+            Breadcrumb(reverse('past_games_url'), 'Past games'),
+            Breadcrumb(reverse('game_details_url', args=[found_game.id]), found_game.when),
+        ],
+    })
--- players/views.py
+++ players/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 from .models import Player
 from django.db.models import Avg, Min, Max, Count
+from django.urls import reverse
 
 # Create your views here.
+
+class Breadcrumb:
+    def __init__(self, path, label):
+        self.path = path
+        self.label = label
+
 def all_players(request):
     found_players = Player.objects.all()
     found_players_aggregation = found_players.aggregate(
@@ -16,4 +23,10 @@ def all_players(request):
 
 def player_details(request, player_id):
     found_player = Player.objects.get(id=player_id)
-    return render(request, 'players/player_details.html', {"player": found_player})
+    return render(request, 'players/player_details.html', {
+        "player": found_player,
+        "breadcrumbs": [
+            Breadcrumb(reverse('all_players_url'), 'All Players'),
+            Breadcrumb(reverse('player_details_url', args=[found_player.id]), found_player.name),
+        ]
+    })
