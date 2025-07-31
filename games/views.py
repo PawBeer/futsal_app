f20c7a735de352781b1bb32715797d5e726810d7
Pawel Beer <pawel.beer@non.keysight.com>
2025-07-31 15:28:02 +0200

    used players with confirmed status instead of slots

--- games/views.py
+++ games/views.py
@@ -32,11 +32,14 @@ def game_details(request, game_id):
     found_game = get_object_or_404(Game, id=game_id)
     slots = [found_game.slot_1, found_game.slot_2, found_game.slot_3, found_game.slot_4, found_game.slot_5, found_game.slot_6, found_game.slot_7, found_game.slot_8, found_game.slot_9, found_game.slot_10 ]
     players = Player.objects.all().order_by('user__username')
+    players_for_game = players.filter(status_history__game_id=game_id)
+    confirmed_players_for_game = players_for_game.filter(status_history__player_status__exact=1)
     found_booking_history = BookingHistoryForGame.objects.filter(game=found_game)
     return render(request, 'games/game_details.html', {
         "game": found_game,
         'players': players,
         'slots': slots,
+        "confirmed_players_for_game": confirmed_players_for_game,
         "booking_history": found_booking_history,
         "breadcrumbs": [
             Breadcrumb(reverse('past_games_url'), 'Past games'),
