6fbe69c3fc8be76a32aa08deaae9823958107227
Pawel Beer <pawel.beer@non.keysight.com>
2025-11-10 20:39:37 +0100

    testcase for add-absence

---
+++ futsal_app/tests/test_add-absence.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from django.test import TestCase
+from django.utils import timezone
+from games.models import PlayerStatusManager, PlayerStatus, Game, BookingHistoryForGame, Player
+from games import views
+from .base import BaseTestCase
+
+
+class SimpleAddAbsenceTest(BaseTestCase):
+
+    def test_add_absence_creates_records(self):
+        player = Player.objects.get(user=self.user_1_per)
+
+        PlayerStatus.objects.create(player_status='resting')
+        game = Game.objects.create(when=datetime(2025, 4, 10), description="Game for absence test")
+
+        data = {
+            'player': player.pk,
+            'date_start': '2025-04-09',
+            'date_end': '2025-04-20',
+            'status': 'resting',
+            'description': 'Simple absence test',
+        }
+        request = self.factory.post('/games/add_absence/', data)
+        request.user = self.superuser
+
+        response = views.add_absence(request)
+
+        self.assertEqual(response.status_code, 302)
+
+        self.assertTrue(PlayerStatusManager.objects.filter(player=player).exists())
+        self.assertTrue(BookingHistoryForGame.objects.filter(player=player, game=game).exists())
