f4edf349f1c12f2ab45c3cf1e5affa51f2d979a9
Pawel Beer <pawel.beer@non.keysight.com>
2025-11-15 17:29:06 +0100

    test #5 fixed

--- futsal_app/tests/test_add_absence.py
+++ futsal_app/tests/test_add_absence.py
@@ -1,33 +1,40 @@
 from datetime import datetime
-from django.test import TestCase
+from django.test import Client, TestCase
 from django.utils import timezone
 from games.models import PlayerStatusManager, Game, BookingHistoryForGame, Player
 from games import views
 from .base import BaseTestCase
-
+from django.urls import reverse
 
 class SimpleAddAbsenceTest(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.client = Client()
 
     def test_add_absence_creates_records(self):
         player = Player.objects.get(user=self.user_1_per)
 
-        game = Game.objects.create(when=datetime(2025, 4, 10), description="Game for absence test")
+        game = Game.objects.create(when=datetime(2025, 11, 16), description="Game for absence test")
+
+        booking_set_user_1_per = BookingHistoryForGame.objects.create(game=game, player=player, player_status='planned')
 
         data = {
             'player': player.pk,
-            'date_start': '2025-04-09',
-            'date_end': '2025-04-20',
+            'date_start': '2025-11-15',
+            'date_end': '2025-11-20',
             'status': 'resting',
             'description': 'Simple absence test',
         }
-        request = self.factory.post('/games/add_absence/', data)
-        request.user = self.superuser
-
-        response = views.add_absence(request)
+        self.client.force_login(self.superuser)
+        url = reverse('add_absence_url')
+        response = self.client.post(url, data)
 
         self.assertEqual(response.status_code, 302)
 
         self.assertTrue(PlayerStatusManager.objects.filter(player=player).exists())
-        self.assertEqual(PlayerStatusManager.objects.filter(player=player), 'resting')
-        self.assertTrue(BookingHistoryForGame.objects.filter(player=player, game=game).exists())
-        self.assertEqual(game.player_status.player_status, 'cancelled')
+        status_obj = PlayerStatusManager.objects.get(player=player)
+        self.assertEqual(status_obj.player_status, 'resting')
+
+        booking_obj = BookingHistoryForGame.objects.filter(player=player, game=game).last()
+        self.assertIsNotNone(booking_obj)
+        self.assertEqual(booking_obj.player_status, 'cancelled')
