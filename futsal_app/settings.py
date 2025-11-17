c1b378b7cc7e2015723b05a62d59a5e8941d688e
Marcin Żyrkowski <marcin.zyrkowski@tronel.pl>
2025-11-17 09:10:02 +0100

    chore: added defaults if missing the env settings

--- futsal_app/settings.py
+++ futsal_app/settings.py
@@ -135,9 +135,9 @@ DISPLAY_NAME_MODE = "full_name"  # Options: "username" or "full_name"
 EMAIL_BACKEND = env(
     "EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend"
 )
-EMAIL_HOST = env("EMAIL_HOST")
-EMAIL_PORT = env.int("EMAIL_PORT")
-EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS")
-EMAIL_HOST_USER = env("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL")
+EMAIL_HOST = env("EMAIL_HOST", default="localhost")
+EMAIL_PORT = env.int("EMAIL_PORT", default=25)
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=False)
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="user")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="password")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="admin")
